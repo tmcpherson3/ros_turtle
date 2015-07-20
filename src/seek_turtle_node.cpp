2aac9067820cfdbd1fb17a1607bf5088fb26a76b
Timothy McPherson <tmcpherson3@users.noreply.github.com>
2015-07-19 23:07:28 -0700

    formatting and update some comments

--- src/seek_turtle_node.cpp
+++ src/seek_turtle_node.cpp
@@ -1,15 +1,14 @@
 #include "Seeker.h"
-//#include "turtlesim.h" ??
 
 /*
-	This node spawns new turtle to seek another turtle
+	This node spawns new turtle to seek out the original turtle
 */
 
 int main(int argc, char **argv)
 {
 	/* 
 	Create seeker object
-	Constructor will initialize ROS node, subscribe to target pose, and 		advertise the seeker's cmd_vel topic
+	Constructor will initialize ROS node, subscribe to target pose, and advertise the seeker's cmd_vel topic
 	*/
 	ros::init(argc, argv, "seek_turtle_node");
 	Seeker seeker;
